34f81373f64ddb964f0f9687836c778d9b167017
MirkusX <marcusk.kodehode@gmail.com>
2023-03-23 14:29:03 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ThreeCards } from "./Components/ThreeCards";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThreeCards />
     </div>
   );
 }
---
+++ src/Components/ThreeCards.js
@@ -0,0 +1,9 @@
+export const ThreeCards = () => {
+  return (
+    <section>
+      <div>
+        <h1>three cards</h1>
+      </div>
+    </section>
+  );
+};
