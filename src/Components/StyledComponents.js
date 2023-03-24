c8664c28256be477a4340805da3d3c6ec728322a
MirkusX <marcusk.kodehode@gmail.com>
2023-03-24 10:17:22 +0100

    hit em with the bopgunn

--- src/Components/StyledComponents.js
+++ src/Components/StyledComponents.js
@@ -2,4 +2,12 @@ import styled from "styled-components";
 
 export const StyledSection = styled.section`
   display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+`;
+
+export const StyledDiv = styled.div`
+  width: 70%;
+  display: flex;
 `;
--- src/Components/ThreeCards.js
+++ src/Components/ThreeCards.js
@@ -1,19 +1,21 @@
 import { cardInfo } from "./cardInfo";
-import { StyledSection } from "./StyledComponents";
+import { StyledDiv, StyledSection } from "./StyledComponents";
 
 export const ThreeCards = () => {
   return (
     <StyledSection>
-      {cardInfo.map((item, index) => {
-        return (
-          <div key={index} style={{ backgroundColor: item.background }}>
-            <img src={item.icon} />
-            <h1>{item.title}</h1>
-            <p>{item.desc}</p>
-            <button>Learn More</button>
-          </div>
-        );
-      })}
+      <StyledDiv>
+        {cardInfo.map((item, index) => {
+          return (
+            <div key={index} style={{ backgroundColor: item.background }}>
+              <img src={item.icon} />
+              <h1>{item.title}</h1>
+              <p>{item.desc}</p>
+              <button>Learn More</button>
+            </div>
+          );
+        })}
+      </StyledDiv>
     </StyledSection>
   );
 };
