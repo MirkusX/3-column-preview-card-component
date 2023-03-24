d0729f7528b2a83a15c3aa5d73384afeed0b5663
MirkusX <marcusk.kodehode@gmail.com>
2023-03-24 11:21:26 +0100

    buttons styled correct

--- src/Components/StyledComponents.js
+++ src/Components/StyledComponents.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { cardInfo } from "./cardInfo";
 
 export const StyledSection = styled.section`
   display: flex;
@@ -14,19 +13,32 @@ export const StyledDiv = styled.div`
   justify-content: center;
 `;
 
-export const StyledButton = styled.button`
-  background-color: white;
-  padding: 1em 2em;
-  border-radius: 30px;
-  margin-top: 6em;
-  border: white 1px solid;
-`;
-
 export const CardDiv = styled.div`
   padding: 3em;
   width: 20%;
   text-align: left;
   color: white;
+  &:nth-child(1) button {
+    color: #e28525;
+    &:hover {
+      color: white;
+      background-color: transparent;
+    }
+  }
+  &:nth-child(2) button {
+    color: #016972;
+    &:hover {
+      color: white;
+      background-color: transparent;
+    }
+  }
+  &:nth-child(3) button {
+    color: #00403f;
+    &:hover {
+      color: white;
+      background-color: transparent;
+    }
+  }
   &:nth-child(1) {
     border-radius: 10px 0 0 10px;
     background-color: #e28525;
@@ -38,4 +50,13 @@ export const CardDiv = styled.div`
     background-color: #00403f;
     border-radius: 0 10px 10px 0;
   }
+`;
+
+export const StyledButton = styled.button`
+  background-color: white;
+  padding: 1em 2em;
+  border-radius: 30px;
+  margin-top: 6em;
+  border: white 3px solid;
+  font-weight: bold;
 `;
